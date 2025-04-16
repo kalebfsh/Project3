f817161b6ad9dccdb77e07374d86d7942ee5c5f0
kalebfsh <kalebcolon@ufl.edu>
2025-04-16 19:13:23 -0400

    Imported dataset and created Song class along with placeholder variables

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+using namespace std;
+
+class Song { // we could use a class to store songs and all their data when the csv file is read and have some storage for all of these
+    //these are really just placeholders these are all the diff data points they got, prob won't need all of them
+    int serial, popularity, track_length, key, time_signature;
+    string track_id, artists, album, track_name, genre;
+    bool potty_words, mode;
+    float danceability, energy, loudness, speechiness, acousticness, liveness, valence, tempo;
+    double instrumentalness;
+};
+// then playlist class or something that creates sorted playlist based on prompt given
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "hey buds\n\n";
+    ifstream file("dataset.csv");
+    if (file.is_open()){
+        string line;
+        getline(file, line);
+        cout << line << endl;
+    }
+
     return 0;
 }
